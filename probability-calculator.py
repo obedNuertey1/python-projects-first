dec6d39dbb384d3ad4ea2c78e466f4a81a3c5777
Obed Nuertey <onuertey1997@gmail.com>
2020-03-04 07:57:54 +0000

    Refactor: Remove unused `__repr__` method and fix `__str__`
    The `__repr__` method was redundant and `__str__` was not implemented correctly. This commit removes `__repr__` and implements `__str__` to provide a useful string representation of the Hat object.

--- probability-calculator.py
+++ probability-calculator.py
@@ -13,3 +13,12 @@ class Hat:
             temp = [value for i in range(key)]  # Create a list of `value` repeated `key` times.
             self.contents += temp  # Add these balls to the contents list.
 
+    def __get_new_entries(self):
+        # Recalculate the `entries` dictionary to reflect the current state of `contents`.
+        new_dict = {key: 0 for key, value in self.entries.items()}  # Initialize all counts to zero.
+        for item in self.contents:
+            new_dict[item] += 1  # Count occurrences of each ball color in `contents`.
+        self.entries = new_dict  # Update the entries to reflect current state.
+    
+    def __str__(self):
+        # Custom string representation for debugging or printing the Hat object.
