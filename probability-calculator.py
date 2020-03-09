2a1ab84676318424567245b40e0351b2a99f479a
Obed Nuertey <onuertey1997@gmail.com>
2020-03-09 21:53:26 +0000

    Refactor: Implement Hat class and draw method
    Implements `Hat` class with `draw` method for random ball drawing. Refactors `experiment` function to utilize the new class.

--- probability-calculator.py
+++ probability-calculator.py
@@ -62,3 +62,10 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     success_count = 0
 
     for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+
+        drawn_dict = {}
+        for ball in drawn_balls:
+            drawn_dict[ball] = drawn_dict.get(ball, 0) + 1
+
