d6039a963f16f4b10e0b07289734f8878ff4a2a0
Obed Nuertey <onuertey1997@gmail.com>
2020-03-13 19:39:00 +0000

    Refactor: Simplify `next_day_in_week` function
    The `next_day_in_week` function is simplified by removing redundant calculations and directly returning the comment or day of the week.

--- time_calculator.py
+++ time_calculator.py
@@ -9,3 +9,10 @@ def next_day_in_week(num, current_day):
     elif num >= 2:
         comment = f' ({num} days later)'
 
+    if not current_day:
+        return comment
+    
+    if current_day:
+        comma_and_space = ', '
+
+    index_of_current_day = days_of_the_week.index(current_day.lower())
