2fcb4e912865224b529b52ce2a6769a4db8207c3
Obed Nuertey <onuertey1997@gmail.com>
2020-03-14 23:44:59 +0000

    Refactor: Extract Category class and implement __str__ method
    This commit extracts the Category class and implements the `__str__` method to display a formatted string representation of the category object.

--- budget-app.py
+++ budget-app.py
@@ -5,3 +5,10 @@ class Category:
 
     def __str__(self):
         result = f"{self.name.center(30, '*')}\n"
+        for item in self.ledger:
+            amount = "{:.2f}".format(item['amount'])
+            description = item['description'][:23]
+            result += f"{description:<23}{amount:>7}\n"
+        total = self.get_balance()
+        result += f"Total: {total:.2f}"
+        return result
