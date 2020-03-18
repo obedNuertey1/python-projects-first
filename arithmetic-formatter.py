ff9a24aff003bed3894773883cf0cac2aa60fa68
Obed Nuertey <onuertey1997@gmail.com>
2020-03-18 05:25:42 +0000

    Fix: Initialize missing variables and add problem limit
    The commit initializes `first_line` and adds a check to limit the number of problems to 5, preventing potential errors.

---
+++ arithmetic-formatter.py
@@ -0,0 +1,7 @@
+def arithmetic_arranger(problems, display_answers=False):
+    # Limit of problems
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    # Containers for the arranged output
+    first_line = []
