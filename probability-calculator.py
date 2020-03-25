e248912675fd526501a5d86e302d2da9dc2c0ddc
Obed Nuertey <onuertey1997@gmail.com>
2020-03-25 20:02:05 +0000

    feat: Initialize Hat with contents and entries
    Initializes the `Hat` class with `contents` and `entries` based on keyword arguments. Populates `contents` with balls according to their counts in `entries`.

--- probability-calculator.py
+++ probability-calculator.py
@@ -4,3 +4,12 @@ from inspect import signature
 
 class Hat:
     def __init__(self, **kwargs):
+        # Initialize the Hat object with keyword arguments representing colors and their counts.
+        self.entries = kwargs  # Dictionary of color names as keys and their counts as values.
+        self.contents = []  # List to store all balls by color.
+
+        # Populate the contents list with balls based on their counts.
+        for value, key in self.entries.items():
+            temp = [value for i in range(key)]  # Create a list of `value` repeated `key` times.
+            self.contents += temp  # Add these balls to the contents list.
+
