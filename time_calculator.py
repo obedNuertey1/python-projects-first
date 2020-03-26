df6d7745f61874eac52e8116664e8faef5b6d10c
Obed Nuertey <onuertey1997@gmail.com>
2020-03-26 02:29:40 +0000

    Refactor: Remove day calculation logic from `next_day_in_week`
    The `next_day_in_week` function now only returns a comment based on the number of days passed. The day calculation logic is removed as it's not used in the current implementation.

--- time_calculator.py
+++ time_calculator.py
@@ -2,3 +2,10 @@ days_of_the_week = ["sunday", "monday", "tuesday", "wednesday", "thursday", "fri
 
 def next_day_in_week(num, current_day):
     comment = ''
+    comma_and_space = ''
+    
+    if num == 1:
+        comment = ' (next day)'
+    elif num >= 2:
+        comment = f' ({num} days later)'
+
