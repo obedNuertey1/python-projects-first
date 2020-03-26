42f7ad81de9270e987ee526f1b3890280e468b61
Obed Nuertey <onuertey1997@gmail.com>
2020-03-26 13:28:56 +0000

    Fix: Incomplete create_spend_chart function
    The `create_spend_chart` function was not fully implemented, resulting in an incomplete chart. This commit completes the function to generate a proper spending chart.

--- budget-app.py
+++ budget-app.py
@@ -44,3 +44,11 @@ def create_spend_chart(categories):
     for category in categories:
         spent = sum(-item['amount'] for item in category.ledger if item['amount'] < 0)
         category_spent.append((category.name, spent))
+        total_spent += spent
+
+    # Calculate percentages
+    percentages = [(name, int((spent / total_spent) * 100)) for name, spent in category_spent]
+
+    # Build the chart
+    chart = "Percentage spent by category\n"
+    for i in range(100, -1, -10):
